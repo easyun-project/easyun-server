591a92bab0aab38f9a0deba5d80b6770108b7861
aleck <me@myners.net>
2021-12-11 00:01:08 +0800

    refactor: server_add

--- easyun/modules/mserver/server_add.py
+++ easyun/modules/mserver/server_add.py
@@ -5,6 +5,7 @@
 '''
 import boto3
 from apiflask import Schema, input, output, auth_required
+from apiflask.schemas import EmptySchema 
 from apiflask.fields import Integer, String, List, Dict
 from apiflask.validators import Length, OneOf
 from flask import jsonify
@@ -15,11 +16,11 @@ from . import bp, REGION, FLAG
 
 
 
-# 测试用云服务器参数定义
+# 测试用云服务器参数示例
 Svrargs = {
     "Number" : 1,
-    "ImageId" : "ami-0447addf6c74624eb",
-    "InstanceType" : "t4g.nano",
+    "ImageId" : "ami-083654bd07b5da81d",
+    "InstanceType" : "t3.nano",
     "SubnetId" : "subnet-06bfe659f6ecc2eed",
     "SecurityGroupIds" : ["sg-05df5c8e8396d06e9",],
     "KeyName" : "key_easyun_dev",
@@ -53,25 +54,31 @@ Svrargs = {
 }
 
 class AddSvr(Schema):
-    # name = String(                          #云服务器名称
-    #     required=True, 
-    #     validate=Length(0, 30),
-    #     metadata={'title': 'Name', 'description': 'The Tag:Name of the server.'}
-    # ) 
+    name = String(                          #云服务器名称
+        required=True, 
+        validate=Length(0, 30),
+        example="new_server_name"
+    ) 
     Number = Integer(required=True, example=1)            #新建云服务器数量
-    ImageId = String(required=True, example="ami-0447addf6c74624eb")          #ImageId
-    InstanceType = String(required=True)            #INSTANCE_TYPE
-    SubnetId = String(required=True) 
-    SecurityGroupIds = String(required=True ) 
-    KeyName = String(required=True)
+    ImageId = String(required=True, example="ami-083654bd07b5da81d")          #ImageId
+    InstanceType = String(required=True,example="t3.nano")            #INSTANCE_TYPE
+    SubnetId = String(required=True,example="subnet-06bfe659f6ecc2eed") 
+    SecurityGroupIds = List(String(required=True ,example= "sg-05df5c8e8396d06e9"))
+    KeyName = String(required=True,example="key_easyun_dev")
     BlockDeviceMappings = List(
-        Dict,
+        Dict(
+            example={
+                "DeviceName": "/dev/xvda",
+                "Ebs": {
+                    "DeleteOnTermination": True,
+                    "VolumeType": "gp2",
+                    "VolumeSize": 16
+                }
+            }
+        ),
         required=True
     )
-    TagSpecifications = List(
-        Dict,
-        required=True
-    )
+
 
 class NewSvrSchema(Schema):
     NewSvrId = String()
@@ -80,25 +87,48 @@ class NewSvrSchema(Schema):
 @bp.post('/add')
 @auth_required(auth_token)
 @input(AddSvr)
-@output(NewSvrSchema)
+# @output(NewSvrSchema)
 def add_server(newsvr):
     '''新建云服务器'''
     try:
+        # print(newsvr)
+        TagSpecifications = [
+        {
+        "ResourceType":"instance",
+        "Tags": [
+                {"Key": "Flag", "Value": "Easyun"},
+                {"Key": "Name", "Value": newsvr['name']}
+            ]
+        }
+        ]
         RESOURCE = boto3.resource('ec2', region_name=REGION)
         # server = RESOURCE.create_instances(newsvr)
-        server = RESOURCE.create_instances(
+        servers = RESOURCE.create_instances(
             MaxCount = newsvr['Number'],
             MinCount = newsvr['Number'],
             ImageId = newsvr['ImageId'],
             InstanceType = newsvr['InstanceType'],
             SubnetId = newsvr['SubnetId'],
             SecurityGroupIds = newsvr['SecurityGroupIds'],
+            # SecurityGroupIds = Svrargs['SecurityGroupIds'],
             KeyName = newsvr['KeyName'],
             BlockDeviceMappings = newsvr['BlockDeviceMappings'],
-            TagSpecifications = newsvr['tag_spec']    
+            # TagSpecifications = newsvr['TagSpecifications'],   
+            TagSpecifications = TagSpecifications,   
+            # BlockDeviceMappings = Svrargs['BlockDeviceMappings'],
+            # TagSpecifications = Svrargs['TagSpecifications']    
         )
+            
         response = Result(
-            detail={'NewSvrId':server[0].id}, status_code=3001
+            # detail = servers,
+            detail=[{
+                'SvrId' : server.id,
+                'InsTpye' : server.instance_type,
+                'CreateTime' : server.launch_time,                
+                'State' : server.state["Name"],
+                'PriIP' : server.private_ip_address
+            } for server in servers],
+            status_code=3001
         )
         # server = [{'id':'3131442142'}]
         # response = Result(
