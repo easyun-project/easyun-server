540472ad5f0ae1b0f8a15f076311530d3d8a14d1
aleck <me@myners.net>
2022-01-14 18:52:53 +0800

    feat: get_ins_family fix: ec2_pricelist

--- easyun/modules/mserver/server_parm.py
+++ easyun/modules/mserver/server_parm.py
@@ -8,10 +8,11 @@ from apiflask import Schema, input, output, auth_required
 from apiflask.fields import Integer, String, List, Dict
 from apiflask.validators import Length, OneOf
 from easyun.common.auth import auth_token
+from easyun.common.models import Datacenter
 from easyun.common.result import Result
 from . import bp
-from .ec2_attrs import AMI_Win, AMI_Lnx, Instance_Types, Instance_OS
-import ast, random
+from .ec2_attrs import AMI_Win, AMI_Lnx, Instance_Family, get_familyDes, Instance_OS
+import ast, random, json
 
 
 class ImagesIn(Schema):
@@ -35,7 +36,7 @@ class ImagesIn(Schema):
 @input(ImagesIn)
 # @output()
 def list_images(parm):
-    '''获取可用的AMI列表和系统盘信息'''
+    '''获取可用的AMI列表(包含 System Disk信息)'''
     if parm['imgPlatform'] == 'windows':
         ami_list = AMI_Win
         def split_name(imgName):
@@ -62,21 +63,23 @@ def list_images(parm):
             Filters = filters
         )
 
+        imgList = [
+            {
+                # image properties
+                'imgID': img['ImageId'],
+                'imgName': [ami['imgName'] for ami in ami_list if ami['amiName']==img['Name']][0],
+                'imgVersion' : [ami['imgVersion'] for ami in ami_list if ami['amiName']==img['Name']][0],
+                'imgCode': [ami['imgCode'] for ami in ami_list if ami['amiName']==img['Name']][0],
+                'imgDescription': img['Description'],
+                # disk parameters        
+                'root_device_name': img['RootDeviceName'],
+                'root_device_type': img['RootDeviceType'],
+                'root_device_disk': img['BlockDeviceMappings'][0]['Ebs']
+            } for img in result['Images']
+        ]
         resp = Result(
-            detail=[
-                {
-                    # image properties
-                    'imgID': img['ImageId'],
-                    'imgName': [ami['imgName'] for ami in ami_list if ami['amiName']==img['Name']][0],
-                    'imgVersion' : [ami['imgVersion'] for ami in ami_list if ami['amiName']==img['Name']][0],
-                    'imgCode': [ami['imgCode'] for ami in ami_list if ami['amiName']==img['Name']][0],
-                    'imgDescription': img['Description'],
-                    # disk parameters        
-                    'root_device_name': img['RootDeviceName'],
-                    'root_device_type': img['RootDeviceType'],
-                    'root_device_disk': img['BlockDeviceMappings'][0]['Ebs']
-                } for img in result['Images']
-            ],
+            detail=imgList,
+            message = "success,"+ str(len(imgList)),
             status_code=200
         )
         return resp.make_resp()
@@ -85,18 +88,73 @@ def list_images(parm):
         response = Result(
             message= ex, status_code=3010
         )
-        response.make_resp()
+        return response.make_resp()
+
+
+
+
+@bp.get('/<dcname>/<arch>/insfamily')
+@auth_required(auth_token)
+def get_ins_family(dcname,arch):
+    '''获取可用的Instance Family列表'''
+    filters=[
+        {'Name': 'processor-info.supported-architecture', 'Values': [arch]}, 
+        {'Name': 'current-generation', 'Values': ['true']},
+    ]    
+    try:
+        # this_dc = Datacenter(name=dcname)
+        this_dc = Datacenter.query.filter_by(name=dcname).first()
+        dcRegion = this_dc.get_region()
+        client_ec2 = boto3.client('ec2', region_name=dcRegion)
+        # instypes = client_ec2.describe_instance_types(
+        #     #InstanceTypes=['t2.micro']
+        #     Filters = filters,
+        # )  
+        desc_args = {}
+        instypeList = []
+        while True:
+            result = client_ec2.describe_instance_types(
+                **desc_args,
+                Filters = filters,
+            )
+    #         print(describe_result.keys())
+            for i in result['InstanceTypes']:
+                insType = i['InstanceType']
+                insFamily = insType.split('.')[0]
+                tmp = {
+                    'insType': insType,
+                    'insFamily': insFamily,
+                    'insCatg': get_familyDes(insFamily)
+                }
+                instypeList.append(tmp)
+            if 'NextToken' not in result:
+                break
+            desc_args['NextToken'] = result['NextToken']
+        # return instypes
+        # print(ec2_types)
+        response = Result(
+            detail = instypeList,
+            message = "success,"+ str(len(instypeList)),
+            status_code=200
+        )
+        return response.make_resp()
+
+    except Exception as ex:
+        response = Result(
+            message= ex, status_code=3020
+        )
+        return response.err_resp()
 
 
 # 定义 insFamily 有效取值范围
-Instance_Family = ['all']
-for i in Instance_Types:
-    Instance_Family.extend(i['insFamily'])
+InsFamily_All = ['all']
+for i in Instance_Family:
+    familyList = [f['familyName'] for f in i['insFamily']]
+    InsFamily_All.extend(familyList)
 
 class InstypesIn(Schema):
     # datacenter basic parm
     dcRegion = String(required=True, example="us-east-1")
-
     insArch = String(
         required=True, 
         validate=OneOf(['x86_64', 'arm64']),  #The CPU architecture ( x86_64 | arm64 )
@@ -104,7 +162,7 @@ class InstypesIn(Schema):
     )
     insFamily = String( 
         required=True, 
-        validate=OneOf(Instance_Family),
+        validate=OneOf(InsFamily_All),
         example="c4"
     )
     imgCode = String(
@@ -116,7 +174,7 @@ class InstypesIn(Schema):
 @auth_required(auth_token)
 @input(InstypesIn)
 def list_ins_types(parm):
-    '''获取可用的Instance Types及价格列表'''    
+    '''获取可用的Instance Types列表(含月度成本)'''    
     family = parm['insFamily']
     arch = parm['insArch']
     imgCode = parm['imgCode']
@@ -155,7 +213,7 @@ def list_ins_types(parm):
         #     Filters = filters,
         # )  
         describe_args = {}
-        ins_types = []
+        instypeList = []
         while True:
             result = client_ec2.describe_instance_types(
                 **describe_args,
@@ -172,14 +230,15 @@ def list_ins_types(parm):
                     "Network": i['NetworkInfo']['NetworkPerformance'],                    
                     "Price": ec2_monthly_cost(parm['dcRegion'],i['InstanceType'], insOS),
                 }
-                ins_types.append(tmp)
+                instypeList.append(tmp)
             if 'NextToken' not in result:
                 break
             describe_args['NextToken'] = result['NextToken']
         # return instypes
         # print(ec2_types)
         response = Result(
-            detail = ins_types,
+            detail = instypeList,
+            message = "success,"+ str(len(instypeList)),
             status_code=200
         )
         return response.make_resp()
@@ -193,7 +252,9 @@ def list_ins_types(parm):
 # 获取实例价格功能实现部分
 def ec2_pricelist(region, instype, os, soft='NA', option='OnDemand',tenancy='Shared'):
     '''获取EC2的价格列表(单位时间Hrs)'''
-    client_price = boto3.client('pricing')
+    # AWS Price API only support us-east-1, ap-south-1
+    priceRegion = 'us-east-1'
+    client_price = boto3.client('pricing', region_name= priceRegion )
     try:
         result = client_price.get_products(
             ServiceCode='AmazonEC2',
@@ -209,8 +270,9 @@ def ec2_pricelist(region, instype, os, soft='NA', option='OnDemand',tenancy='Sha
                 {'Type': 'TERM_MATCH', 'Field': 'preInstalledSw','Value': soft},
             ],
         )
+        # 通过 ast.literal_eval()函数 对字符串进行类型转换
         prod = ast.literal_eval(result['PriceList'][0])
-        price1 = prod['terms'][option]  
+        price1 = prod['terms'].get(option)  
         key1 = list(price1.keys())[0]
         price2 = price1[key1]['priceDimensions']
         key2 = list(price2.keys())[0]
@@ -220,6 +282,7 @@ def ec2_pricelist(region, instype, os, soft='NA', option='OnDemand',tenancy='Sha
         return ex
 
 def ec2_monthly_cost(region, instype, os):
+    '''获取EC2的月度成本(单位时间Month)'''
     if os == 'NA':
         return None
     try:
@@ -228,6 +291,7 @@ def ec2_monthly_cost(region, instype, os):
         currency = list(pricelist['pricePerUnit'].keys())[0]
         unitePrice = pricelist['pricePerUnit'].get(currency)
         if unit == 'Hrs':
+            # 参考AWS做法按每月730h计算
             monthPrice = float(unitePrice)*730
         return {
             'value': monthPrice,
