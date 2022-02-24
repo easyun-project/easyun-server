245b1f79894aa4ea8aec13f89c2c255818be43e5
Qianxi Lu <lqianxi@nwcdcloud.cn>
2022-02-24 10:22:28 +0800

    Object lists

---
+++ easyun/modules/mstorage/api_object_get.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+  @module:  Storage Object Detail
+  @desc:    Get object detail info
+  @auth:    
+"""
+
+import boto3
+from apiflask import Schema, input, output, auth_required
+from apiflask.fields import Integer, String, List, Dict
+from apiflask.validators import Length, OneOf
+from easyun.common.auth import auth_token
+from easyun.common.utils import len_iter, query_dc_region
+from easyun.common.result import Result, make_resp, error_resp, bad_request
+from easyun.common.schemas import DcNameQuery
+from .schemas import BktNameQuery
+from . import bp
+
+@bp.get('/bucket/object/<bkt_id>')
+#@auth_required(auth_token)
+@input(DcNameQuery, location='query')
+def get_bkt_detail(bkt_id, parm):
+    '''获取指定存储桶(Bucket)内对象列表的详细信息'''
+    dcName=parm.get('dc')
+    try:
+        dcRegion =  query_dc_region(dcName)
+        # 设置 boto3 接口默认 region_name
+        boto3.setup_default_session(region_name = dcRegion )
+
+        client_s3 = boto3.client('s3') 
+        objects = client_s3.list_objects_v2()['Contents']
+        objectList = []
+        for obj in objectList:
+            objDict = {
+                'name' : obj['Key'],
+                'size' : obj['Size'],
+                'storageClass' : obj['StorageClass'],
+                'lastModified' : obj['LastModified'].isoformat()
+            }
+            objectList.append(objDict)
+
+        response = Result(
+            detail= objectList,
+            status_code=200
+        )
+        return response.make_resp()
+    except Exception as ex:
+        response = Result(
+            message=str(ex),
+            status_code=4002,
+        )
+        return response.err_resp()
